269422ffc53dfb47b25d6c07f550203a86c8de59
pajagl <114282176+pajagl@users.noreply.github.com>
2023-03-08 19:00:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+// tady je místo pro náš program
+
+
+=======
+// tady je místo pro náš program
+
+let vstup = document.querySelector(".reseni");
+
+let jmeno = prompt("Zadejte své celé jméno");
+
+let rok = Number(prompt("Zadejte svůj rok narození"));
+
+vstup.innerHTML += jmeno + " " + (2022 - rok)
+
+let barva = prompt("Napiš oblíbenou barvu anglicky")
+
+vstup.style.color = barva;
+vystup.style.fontStyle = "italic"
+
+>>>>>>> b9ecbcddddd872f9e9841d075c1b41e012a1d27e
